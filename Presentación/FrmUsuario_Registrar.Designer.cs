a52863a6783e53b61ed33bdb07da65c96f561628
kevin castillo <kevin castillo@DESKTOP-J42G3E9>
2021-10-27 01:04:05 -0500

    cambios

--- Presentación/FrmUsuario_Registrar.Designer.cs
+++ Presentación/FrmUsuario_Registrar.Designer.cs
@@ -57,6 +57,8 @@ namespace Presentación
             this.rjButton1 = new CustomControls.RJControls.RJButton();
             this.error = new System.Windows.Forms.ErrorProvider(this.components);
             this.DTFecha = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CbxEntrenador = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.error)).BeginInit();
             this.SuspendLayout();
@@ -65,6 +67,8 @@ namespace Presentación
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.CbxEntrenador);
+            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label12);
             this.panel1.Controls.Add(this.label11);
             this.panel1.Controls.Add(this.CbxSexo);
@@ -99,11 +103,11 @@ namespace Presentación
             // 
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.Color.Transparent;
-            this.label12.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label12.Location = new System.Drawing.Point(440, 354);
+            this.label12.Location = new System.Drawing.Point(500, 223);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(30, 19);
+            this.label12.Size = new System.Drawing.Size(24, 15);
             this.label12.TabIndex = 25;
             this.label12.Text = "Kg";
             // 
@@ -111,11 +115,11 @@ namespace Presentación
             // 
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.Color.Transparent;
-            this.label11.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label11.Location = new System.Drawing.Point(440, 310);
+            this.label11.Location = new System.Drawing.Point(380, 223);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(30, 19);
+            this.label11.Size = new System.Drawing.Size(22, 15);
             this.label11.TabIndex = 24;
             this.label11.Text = "Mt";
             // 
@@ -129,7 +133,7 @@ namespace Presentación
             "F"});
             this.CbxSexo.Location = new System.Drawing.Point(182, 216);
             this.CbxSexo.Name = "CbxSexo";
-            this.CbxSexo.Size = new System.Drawing.Size(63, 28);
+            this.CbxSexo.Size = new System.Drawing.Size(63, 24);
             this.CbxSexo.TabIndex = 23;
             this.CbxSexo.Validating += new System.ComponentModel.CancelEventHandler(this.CbxSexo_Validating);
             this.CbxSexo.Validated += new System.EventHandler(this.CbxSexo_Validated);
@@ -138,9 +142,9 @@ namespace Presentación
             // 
             this.TxtAltura.BackColor = System.Drawing.Color.Gray;
             this.TxtAltura.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.TxtAltura.Location = new System.Drawing.Point(387, 306);
+            this.TxtAltura.Location = new System.Drawing.Point(327, 216);
             this.TxtAltura.Name = "TxtAltura";
-            this.TxtAltura.Size = new System.Drawing.Size(47, 27);
+            this.TxtAltura.Size = new System.Drawing.Size(47, 23);
             this.TxtAltura.TabIndex = 22;
             this.TxtAltura.Validating += new System.ComponentModel.CancelEventHandler(this.TxtAltura_Validating);
             this.TxtAltura.Validated += new System.EventHandler(this.TxtAltura_Validated);
@@ -149,9 +153,9 @@ namespace Presentación
             // 
             this.TxtPeso.BackColor = System.Drawing.Color.Gray;
             this.TxtPeso.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.TxtPeso.Location = new System.Drawing.Point(387, 348);
+            this.TxtPeso.Location = new System.Drawing.Point(454, 216);
             this.TxtPeso.Name = "TxtPeso";
-            this.TxtPeso.Size = new System.Drawing.Size(47, 27);
+            this.TxtPeso.Size = new System.Drawing.Size(47, 23);
             this.TxtPeso.TabIndex = 21;
             this.TxtPeso.Validating += new System.ComponentModel.CancelEventHandler(this.TxtPeso_Validating);
             this.TxtPeso.Validated += new System.EventHandler(this.TxtPeso_Validated);
@@ -162,9 +166,9 @@ namespace Presentación
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label10.Location = new System.Drawing.Point(323, 353);
+            this.label10.Location = new System.Drawing.Point(408, 220);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(58, 24);
+            this.label10.Size = new System.Drawing.Size(49, 19);
             this.label10.TabIndex = 20;
             this.label10.Text = "Peso";
             // 
@@ -174,9 +178,9 @@ namespace Presentación
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label9.Location = new System.Drawing.Point(323, 307);
+            this.label9.Location = new System.Drawing.Point(272, 220);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(73, 24);
+            this.label9.Size = new System.Drawing.Size(58, 19);
             this.label9.TabIndex = 19;
             this.label9.Text = "Altura";
             // 
@@ -188,7 +192,7 @@ namespace Presentación
             this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label8.Location = new System.Drawing.Point(26, 220);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(61, 24);
+            this.label8.Size = new System.Drawing.Size(51, 19);
             this.label8.TabIndex = 16;
             this.label8.Text = "Sexo";
             // 
@@ -241,7 +245,7 @@ namespace Presentación
             this.CbxPlanes.FormattingEnabled = true;
             this.CbxPlanes.Location = new System.Drawing.Point(182, 348);
             this.CbxPlanes.Name = "CbxPlanes";
-            this.CbxPlanes.Size = new System.Drawing.Size(112, 28);
+            this.CbxPlanes.Size = new System.Drawing.Size(112, 24);
             this.CbxPlanes.TabIndex = 13;
             // 
             // TxtCelular
@@ -250,7 +254,7 @@ namespace Presentación
             this.TxtCelular.Cursor = System.Windows.Forms.Cursors.Hand;
             this.TxtCelular.Location = new System.Drawing.Point(182, 306);
             this.TxtCelular.Name = "TxtCelular";
-            this.TxtCelular.Size = new System.Drawing.Size(112, 27);
+            this.TxtCelular.Size = new System.Drawing.Size(112, 23);
             this.TxtCelular.TabIndex = 12;
             this.TxtCelular.Validating += new System.ComponentModel.CancelEventHandler(this.TxtCelular_Validating);
             this.TxtCelular.Validated += new System.EventHandler(this.TxtCelular_Validated);
@@ -261,7 +265,7 @@ namespace Presentación
             this.TxtApellido.Cursor = System.Windows.Forms.Cursors.Hand;
             this.TxtApellido.Location = new System.Drawing.Point(182, 174);
             this.TxtApellido.Name = "TxtApellido";
-            this.TxtApellido.Size = new System.Drawing.Size(275, 27);
+            this.TxtApellido.Size = new System.Drawing.Size(275, 23);
             this.TxtApellido.TabIndex = 10;
             this.TxtApellido.Validating += new System.ComponentModel.CancelEventHandler(this.TxtApellido_Validating);
             this.TxtApellido.Validated += new System.EventHandler(this.TxtApellido_Validated);
@@ -272,7 +276,7 @@ namespace Presentación
             this.TxtNombre.Cursor = System.Windows.Forms.Cursors.Hand;
             this.TxtNombre.Location = new System.Drawing.Point(182, 130);
             this.TxtNombre.Name = "TxtNombre";
-            this.TxtNombre.Size = new System.Drawing.Size(275, 27);
+            this.TxtNombre.Size = new System.Drawing.Size(275, 23);
             this.TxtNombre.TabIndex = 10;
             this.TxtNombre.Validating += new System.ComponentModel.CancelEventHandler(this.TxtNombre_Validating);
             this.TxtNombre.Validated += new System.EventHandler(this.TxtNombre_Validated);
@@ -283,7 +287,7 @@ namespace Presentación
             this.TxtIdentificacion.Cursor = System.Windows.Forms.Cursors.Hand;
             this.TxtIdentificacion.Location = new System.Drawing.Point(182, 86);
             this.TxtIdentificacion.Name = "TxtIdentificacion";
-            this.TxtIdentificacion.Size = new System.Drawing.Size(133, 27);
+            this.TxtIdentificacion.Size = new System.Drawing.Size(133, 23);
             this.TxtIdentificacion.TabIndex = 9;
             this.TxtIdentificacion.Validating += new System.ComponentModel.CancelEventHandler(this.TxtIdentificacion_Validating);
             this.TxtIdentificacion.Validated += new System.EventHandler(this.TxtIdentificacion_Validated);
@@ -296,7 +300,7 @@ namespace Presentación
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label7.Location = new System.Drawing.Point(26, 352);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(54, 24);
+            this.label7.Size = new System.Drawing.Size(43, 19);
             this.label7.TabIndex = 8;
             this.label7.Text = "Plan";
             // 
@@ -308,7 +312,7 @@ namespace Presentación
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label6.Location = new System.Drawing.Point(26, 306);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(84, 24);
+            this.label6.Size = new System.Drawing.Size(66, 19);
             this.label6.TabIndex = 7;
             this.label6.Text = "Celular";
             // 
@@ -320,7 +324,7 @@ namespace Presentación
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label5.Location = new System.Drawing.Point(26, 262);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(226, 24);
+            this.label5.Size = new System.Drawing.Size(181, 19);
             this.label5.TabIndex = 6;
             this.label5.Text = "Fecha de nacimiento";
             // 
@@ -332,7 +336,7 @@ namespace Presentación
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.Location = new System.Drawing.Point(26, 86);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(154, 24);
+            this.label3.Size = new System.Drawing.Size(119, 19);
             this.label3.TabIndex = 4;
             this.label3.Text = "Identificación";
             // 
@@ -344,7 +348,7 @@ namespace Presentación
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label2.Location = new System.Drawing.Point(26, 177);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 24);
+            this.label2.Size = new System.Drawing.Size(82, 19);
             this.label2.TabIndex = 3;
             this.label2.Text = "Apellidos";
             // 
@@ -356,7 +360,7 @@ namespace Presentación
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(26, 133);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 24);
+            this.label1.Size = new System.Drawing.Size(84, 19);
             this.label1.TabIndex = 2;
             this.label1.Text = "Nombres";
             // 
@@ -405,12 +409,34 @@ namespace Presentación
             // 
             this.DTFecha.Location = new System.Drawing.Point(394, 328);
             this.DTFecha.Name = "DTFecha";
-            this.DTFecha.Size = new System.Drawing.Size(266, 27);
+            this.DTFecha.Size = new System.Drawing.Size(266, 23);
             this.DTFecha.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(308, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 19);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Entrenador";
+            // 
+            // CbxEntrenador
+            // 
+            this.CbxEntrenador.BackColor = System.Drawing.Color.Gray;
+            this.CbxEntrenador.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CbxEntrenador.FormattingEnabled = true;
+            this.CbxEntrenador.Location = new System.Drawing.Point(412, 305);
+            this.CbxEntrenador.Name = "CbxEntrenador";
+            this.CbxEntrenador.Size = new System.Drawing.Size(101, 24);
+            this.CbxEntrenador.TabIndex = 27;
             // 
             // FrmUsuario_Registrar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(131)))), ((int)(((byte)(149)))));
             this.ClientSize = new System.Drawing.Size(899, 557);
@@ -458,5 +484,7 @@ namespace Presentación
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.DateTimePicker DTFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CbxEntrenador;
     }
 }
